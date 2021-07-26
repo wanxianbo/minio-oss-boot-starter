013a05dc603224c6079473729ed9a69227b8854b
PrideWan <1036700294@qq.com>
2021-07-26 22:42:12 +0800

    init ossClient and api

--- src/main/java/com/wanxianbo/oss/minio/MinioOssApi.java
+++ src/main/java/com/wanxianbo/oss/minio/MinioOssApi.java
@@ -1,18 +1,34 @@
 package com.wanxianbo.oss.minio;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.io.ByteSource;
+import com.wanxianbo.oss.minio.config.CustomAssumeRoleProvider;
+import com.wanxianbo.oss.minio.config.CustomCredentials;
 import com.wanxianbo.oss.minio.config.MinioProperties;
 import com.wanxianbo.oss.minio.policy.PolicyFile;
 import com.wanxianbo.oss.minio.policy.Statement;
-import io.minio.GetPresignedObjectUrlArgs;
-import io.minio.MinioClient;
-import io.minio.UploadObjectArgs;
+import io.minio.*;
 import io.minio.credentials.StaticProvider;
+import io.minio.errors.ErrorResponseException;
 import io.minio.http.Method;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.slf4j.Logger;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Nullable;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.util.Base64;
+import java.util.Map;
+
+import static java.util.Optional.ofNullable;
 
 /**
  * <p>
@@ -23,7 +39,7 @@ import javax.annotation.Nullable;
  * @date 2021/7/26
  */
 @EnableConfigurationProperties(value = MinioProperties.class)
-//@Slf4j
+@Slf4j
 public class MinioOssApi {
 
     private static final Integer DEFAULT_PART_SIZE = 10 * 1024 * 1024;
@@ -64,10 +80,154 @@ public class MinioOssApi {
 
     }
 
+    public MinioOssToken temporaryOssToken() {
+        try {
+            CustomAssumeRoleProvider roleProvider = new CustomAssumeRoleProvider(
+                    properties.getEndpoint(),
+                    properties.getKey(),
+                    properties.getSecret(),
+                    (int) properties.getTokenDuration().getSeconds(),
+                    policy, properties.getRegion(), null, "DefaultSession", null, null
+            );
+            return MinioOssToken.from((CustomCredentials) roleProvider.fetch(), properties);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("not expected error:", e);
+        }
+    }
+
 
     public String generateObjectKey(String id, boolean isPublic, @Nullable String prefix, @Nullable String suffix) {
-        return null;
+        String info = String.format("new-oss:%s:%s", isPublic ? "public" : "private", id);
+        String base64 = Base64.getUrlEncoder().withoutPadding().encodeToString(info.getBytes());
+        StringBuilder builder = new StringBuilder();
+        if (!Strings.isNullOrEmpty(prefix)) builder.append(prefix).append("__");
+        builder.append(base64);
+        if (!Strings.isNullOrEmpty(suffix)) builder.append(".").append(suffix);
+        return builder.toString();
     }
+
+    public Map<String, String> presignedFormData(String objectName, PostPolicy updatePolicy)
+            throws ErrorResponseException {
+        Preconditions.checkState(!Strings.isNullOrEmpty(objectName));
+        Preconditions.checkNotNull(updatePolicy);
+        updatePolicy.addEqualsCondition("key", objectName);
+        try {
+            Map<String, String> result = client.getPresignedPostFormData(updatePolicy);
+            result.put("bucketUrl", UriComponentsBuilder
+                    .fromHttpUrl(properties.getEndpoint())
+                    .path(updatePolicy.bucket()).toUriString());
+            result.put("bucket", updatePolicy.bucket());
+            result.put("key", objectName);
+            return result;
+        } catch (ErrorResponseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String presignedObjectUrl(String objectName, @Nullable String bucketName, @Nullable Duration duration)
+            throws ErrorResponseException {
+        try {
+            GetPresignedObjectUrlArgs.Builder builder = GetPresignedObjectUrlArgs.builder()
+                    .method(Method.GET).object(objectName)
+                    .region(properties.getRegion())
+                    .bucket(ofNullable(bucketName).orElseGet(properties::getBucket));
+            ofNullable(duration).map(Duration::getSeconds).map(Long::intValue).ifPresent(builder::expiry);
+            return client.getPresignedObjectUrl(builder.build());
+        } catch (ErrorResponseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public MinioClient getClient() {
+        return client;
+    }
+
+    public ObjectWriteResponse uploadData(InputStream stream, String objectName) throws ErrorResponseException {
+        return uploadData(stream, objectName, null, null);
+    }
+
+    public ObjectWriteResponse uploadData(InputStream stream, String objectName,
+                                          @Nullable String contentType,
+                                          @Nullable String bucketName) throws ErrorResponseException {
+        try {
+            int size = stream.available();
+            int partSize = -1;
+            if (size == 0) {
+                size = -1;
+                partSize = DEFAULT_PART_SIZE;
+            }
+            PutObjectArgs.Builder builder = PutObjectArgs.builder()
+                    .preloadData(true)
+                    .object(objectName)
+                    .bucket(nullableBucket(bucketName))
+                    .stream(stream, size, partSize);
+            ofNullable(contentType).ifPresent(builder::contentType);
+            return client.putObject(builder.build());
+        } catch (ErrorResponseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void downloadData(OutputStream stream, String objectName, @Nullable String bucketName)
+            throws ErrorResponseException {
+        try {
+            GetObjectResponse response = client.getObject(GetObjectArgs.builder()
+                    .object(objectName)
+                    .bucket(nullableBucket(bucketName)).build());
+            copyTo(response, stream);
+        } catch (ErrorResponseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void copyTo(InputStream input, OutputStream output) throws IOException {
+        if (!(output instanceof BufferedOutputStream))
+            output = new BufferedOutputStream(output);
+        new ByteSource() {
+            @Override
+            public InputStream openStream() {
+                return input;
+            }
+        }.copyTo(output);
+        output.flush();
+    }
+
+    public static Logger getLog() {
+        return log;
+    }
+
+    public ObjectWriteResponse uploadLargeData(InputStream stream, String objectName,
+                                               @Nullable String contentType,
+                                               @Nullable Integer partSize,
+                                               @Nullable String bucketName) throws ErrorResponseException {
+
+        PutObjectArgs.Builder builder = PutObjectArgs.builder()
+                .preloadData(true)
+                .stream(stream, -1, ofNullable(partSize).orElse(DEFAULT_PART_SIZE))
+                .bucket(nullableBucket(bucketName))
+                .object(objectName);
+        ofNullable(contentType).ifPresent(builder::contentType);
+        try {
+            return client.putObject(builder.build());
+        } catch (ErrorResponseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String nullableBucket(String nullable) {
+        return ofNullable(nullable).orElseGet(properties::getBucket);
+    }
+
 
 
     public static void main(String[] args) throws Exception {
